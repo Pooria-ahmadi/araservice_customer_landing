e83bdb404b0b0247792917280e774d8e8a93b246
Pooria-ahmadi <pa2002.tir@gmail.com>
2025-02-12 00:22:26 +0330

    add project in github

---
+++ components/menudesktop.vue
@@ -0,0 +1,151 @@
+<template>
+  <nav class="navbar is-flex is-justify-content-space-between py-1 px-3">
+    <div class="container">
+      <div class="navbar-content is-flex is-align-items-center">
+        <div class="navbar-burger" :class="{ 'is-active': isMenuOpen }" @click="toggleMenu">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <div class="navbar-item left-logo">
+          <img src="/logo right.png" alt="Logo" class="logo">
+        </div>
+        <div class="navbar-menu" :class="{ 'is-active': isMenuOpen }">
+          <div class="navbar-start is-justify-content-center w-100">
+            <NuxtLink to="/" class=" mx-5 mt-10px color-blue-dark" href="#">
+              صفحه اصلی
+            </NuxtLink>
+            <NuxtLink to="/" class=" mx-5 mt-10px has-text-black is-flex is-align-items-center" href="#">
+              <p>
+                نصب اپلیکیشن
+              </p>
+              <img src="Chevron down.png" alt="">
+            </NuxtLink>
+            <NuxtLink to="/" class=" mx-5 mt-10px has-text-black is-flex is-align-items-center" href="#">
+              ثبت نام تعمیرکاران
+            </NuxtLink>
+            <NuxtLink to="/" class=" mx-5 mt-10px has-text-black is-flex is-align-items-center" href="#">
+              درباره آراسرویس
+            </NuxtLink>
+          </div>
+        </div>
+        <div class="navbar-item right-logo">
+          <img src="/logo left.png" alt="Right Image" class="logo">
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isMenuOpen: false
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.navbar {
+  position: fixed;
+  top: 10px;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: white;
+  max-width: 1000px;
+  width: 100%;
+  border-radius: 15px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+  z-index: 1000;
+}
+
+.logo {
+  max-height: 50px;
+}
+
+.navbar-burger {
+  display: none;
+  cursor: pointer;
+}
+
+.navbar-burger span {
+  display: block;
+  width: 25px;
+  height: 3px;
+  margin: 5px auto;
+  background-color: black;
+}
+
+.navbar-menu {
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+}
+
+@media screen and (max-width: 768px) {
+  .navbar {
+    max-width: 93%;
+  }
+
+  .navbar-content {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    position: relative;
+  }
+
+  .navbar-burger {
+    display: block;
+    order: 1;
+  }
+
+  .left-logo {
+    order: 2;
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  .right-logo {
+    order: 3;
+  }
+
+  .navbar-menu {
+    display: none;
+    position: absolute;
+    top: 100%;
+    left: 0;
+    width: 100%;
+    background: white;
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+    border-radius: 0 0 15px 15px;
+    padding: 10px 0;
+    z-index: 999;
+  }
+
+  .navbar-menu.is-active {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .navbar-menu .navbar-item {
+    width: 100%;
+    text-align: center;
+    padding: 10px;
+    border-bottom: 1px solid #ddd;
+  }
+
+  .navbar-menu .navbar-item:last-child {
+    border-bottom: none;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <div class="background-container">
+      <div class="text-content has-text-centered w-295px is-hidden-mobile">
+        <h1 class=" is-size-3-desktop is-size-5-mobile color-blue-dark has-text-weight-bold">
+          تعمیرات سریع و مطمئن<br>
+          خانه‌تان را به ما بسپارید!
+        </h1>
+        <p class=" is-size-7 mt-6 has-text-gray">
+          تعمیرات سریع و مطمئن در دستان شماست! با ما به راحتی مشکلات خود را حل کنید. از مشاوره تا انجام خدمات، همه چیز
+          به صورت آنلاین و در کمترین زمان ممکن
+        </p>
+        <p class=" mt-2 font-bold-number">
+          تعمیرات لوازم خانگی، و تجهیزات صنعتی با کیفیت بالا و قیمت مناسب
+        </p>
+        <button class="button bg-blue-dark br-15px px-5 mt-5 w-145px">
+          <div class="is-align-items-center is-flex">
+            <p class="has-text-white ml-3">
+              ورود
+            </p>
+            <img src="left white.png" alt="">
+          </div>
+        </button>
+      </div>
+    </div>
+    <div class="has-text-centered is-hidden-mobile">
+      <img src="image 14.png" alt="">
+    </div>
+    <div class="has-text-centered is-hidden-desktop is-hidden-tablet">
+      <img src="Frame 146.png" alt="">
+    </div>
+
+
+    <div class="w-900px mx-auto mt-5 ">
+      <div class="columns mx-2 is-centered mt-6">
+        <div class="column  has-text-centered">
+          <p class="color-blue-dark  font-bold-number is-size-3-desktop  is-size-5-mobile">
+            خدمات آنلاین تعمیرات حرفه‌ای
+          </p>
+
+          <p class=" mt-5 mb-3">
+            اپلیکیشن‌های خدمات تعمیرات آنلاین پلتفرم‌هایی هستند که به کاربران این امکان را می‌دهند تا به راحتی و تنها با
+            چند کلیک، نیازهای تعمیراتی خود را برطرف کنند.
+
+          </p>
+          <p>
+            این اپلیکیشن‌ها معمولاً شامل دسته‌بندی‌های مختلفی از جمله تعمیر لوازم خانگی، خودرو، الکترونیک و حتی خدمات
+            ساختمانی هستند. کاربران می‌توانند با انتخاب نوع خدمت مورد نظر، اطلاعات مربوط به تکنسین‌ها یا متخصصان را
+            مشاهده
+            کرده و با آن‌ها ارتباط برقرار کنند.
+          </p>
+          <button class="button bg-blue-dark br-15px  mt-5 w-164px">
+            <div class="is-align-items-center is-flex">
+              <p class="has-text-white ml-3">
+                نصب اپلیکیشن‌
+              </p>
+              <img src="left white.png" alt="">
+            </div>
+          </button>
+        </div>
+      </div>
+    </div>
+    <servicedesktop />
+    <servicemobile />
+    <div class="w-900px mx-auto ">
+      <div class="columns mx-0 is-centered mt-5">
+        <div class="column is-9 has-text-centered">
+          <p>
+            بهبود تجربه مشتری، افزایش رضایت کاربران و در نهایت رشد اقتصادی در این حوزه شود. همچنین، با توجه به نیازهای
+            روزافزون جامعه به خدمات سریع و قابل اعتماد، این اپلیکیشن‌ها می‌توانند به عنوان یک پل ارتباطی مؤثر بین
+            ارائه‌دهندگان خدمات و مصرف‌کنندگان عمل کنند و به ایجاد شفافیت و اعتماد در بازار کمک نمایند.
+          </p>
+          <p class="mt-5">
+            در نتیجه، توسعه این پلتفرم‌ها نه تنها به نفع کاربران خواهد بود بلکه می‌تواند فرصت‌های شغلی جدیدی نیز ایجاد
+            کند و
+            به رونق کسب‌وکارهای کوچک و متوسط کمک کند.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import servicedesktop from "@/components/servicedesktop.vue";
+import servicemobile from "@/components/servicemobile.vue";
+
+export default {
+  components: {
+    servicedesktop,
+    servicemobile,
+  },
+};
+</script>
